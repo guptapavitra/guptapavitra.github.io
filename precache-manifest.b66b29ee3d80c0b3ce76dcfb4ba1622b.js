a50368aa6157fea31f4210c204f6712a3304916c
Pavitra Gupta <gupta.pavitra@gmail.com>
2019-02-08 21:13:51 +0530

    Added composer.json to run the app on heroku

---
+++ precache-manifest.b66b29ee3d80c0b3ce76dcfb4ba1622b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b4269e3be6379b293342",
+    "url": "/static/js/main.b4269e3b.chunk.js"
+  },
+  {
+    "revision": "02cd35ada11873426425",
+    "url": "/static/js/1.02cd35ad.chunk.js"
+  },
+  {
+    "revision": "b4269e3be6379b293342",
+    "url": "/static/css/main.aa005745.chunk.css"
+  },
+  {
+    "revision": "a05f7a4f5f527590b93d8fbe33b92231",
+    "url": "/index.html"
+  }
+];
